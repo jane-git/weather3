bf7f9fcb83ba1df24842aece93c16ab08323b8d8
jane <blue70677@gmail.com>
2021-12-21 13:35:38 +1000

    change to row (direction of flex container)

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     //layout size - To match the react/Adjust the size equally
-    //default view : flex container.
-    <View style ={{flex:1}}> 
+    //default view : flex container
+    //default of direction of flex container : column
+    //change to row (direction of flex container)
+    <View style ={{flex:1, flexDirection: "row"}}> 
       <View style={{flex:1, backgroundColor:"tomato"}}> </View>
       <View style={{flex:1, backgroundColor:"teal"}}> </View>
       <View style={{flex:1, backgroundColor:"orange"}}> </View>
